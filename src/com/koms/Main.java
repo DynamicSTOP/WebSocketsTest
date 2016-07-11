15e6b3c16b3f88021587e60986eae2e7864dc676
Leonid Babikov <koms@softgrad.com>
2016-07-11 09:53:34 +0300

    git ignore exit

--- src/com/koms/Main.java
+++ src/com/koms/Main.java
@@ -1,8 +1,18 @@
 package com.koms;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws InterruptedException, IOException {
         System.out.println("Hallo");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            if (System.in.available() > 0)
+                if (br.readLine().equals("exit"))
+                    break;
+        }
+        System.out.println("Stopping");
     }
 }
